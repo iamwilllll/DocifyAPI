8844966f61f53a14436bfdb44f52c2cf2795c439
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-03-27 22:31:15 -0400

    Add createSection and updateSection middlewares for section management functionality

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,6 @@
 export * from './errorMiddleware.js';
 export * from './handleInputError.js';
+
+// *sections-specific middlewares
+export * from './sections/createSection.middleware.js';
+export * from './sections/updateSection.middleware.js';

2fecdc2b09cb3bfaa68b3a687bca552cf14bd12a
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-03-27 22:23:43 -0400

    Add getAllSections and getSectionById controllers for section retrieval functionality

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,4 @@
 export * from './sections/createSection.controller.js';
 export * from './sections/updateSection.controller.js';
+export * from './sections/getAllSections.controller.js';
+export * from './sections/getSectionById.controller.js';
